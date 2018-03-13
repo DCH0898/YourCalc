c3daba368efee31c381d59c6918caafc86fb7a2b
chun <lidachun0898@126.com>
2018-03-14 01:29:14 +0800

    添加文件

--- app/src/main/java/com/chun/calc/BeanTwo.java
+++ app/src/main/java/com/chun/calc/BeanTwo.java
@@ -3,6 +3,59 @@ package com.chun.calc;
 /**
  * Created by Administrator on 2018/3/12.
  */
-
 public class BeanTwo {
+    private double valuation;
+    private double share;
+    private double quota;
+    private double profit;
+    private String title;
+    private String notice;
+
+    public double getValuation() {
+        return valuation;
+    }
+
+    public void setValuation(double valuation) {
+        this.valuation = valuation;
+    }
+
+    public double getShare() {
+        return share;
+    }
+
+    public void setShare(double share) {
+        this.share = share;
+    }
+
+    public double getQuota() {
+        return quota;
+    }
+
+    public void setQuota(double quota) {
+        this.quota = quota;
+    }
+
+    public double getProfit() {
+        return profit;
+    }
+
+    public void setProfit(double profit) {
+        this.profit = profit;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNotice() {
+        return notice;
+    }
+
+    public void setNotice(String notice) {
+        this.notice = notice;
+    }
 }
